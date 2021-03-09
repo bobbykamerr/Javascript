c126d7a11a61dede15badb39ae9dd690cf302f1c
bobbykamerr <bobby.kamer@student.rocvf.nl>
2021-03-09 14:10:22 +0100

    h2

---
+++ H2/h2.js
@@ -0,0 +1,5 @@
+document.getElementById("paginatitel").innerText="mijn pagina"
+
+document.getElementById("subkop").innerText="subkop"
+document.getElementById("aap1").src="aap1.jpg"
+document.getElementById("aap2").src="aap2.jpg"
